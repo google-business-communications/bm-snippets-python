bc2910873dca60a9ccc16e9537817c1b589f7e17
Felipe Hlibco <felipehlibco@google.com>
2021-12-20 11:43:31 -0800

    add copyright, intro header and link to docs
    Change-Id: I6d233ef15bbadec6094eea9376a98002c1e4d6aa

--- send-message-live-agent-request-suggestion.py
+++ send-message-live-agent-request-suggestion.py
@@ -1,3 +1,21 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This code sends a text message to the user with a Live agent request suggestion
+# that allows the user to connect with a Live agent.
+# Read more: https://developers.google.com/business-communications/business-messages/guides/how-to/message/send?hl=en#live_agent_request_suggestion
+
 # This code is based on the https://github.com/google-business-communications/python-businessmessages
 # Python Business Messages client library.
 
